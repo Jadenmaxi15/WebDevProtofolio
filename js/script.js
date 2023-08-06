42ecc8292cc347f9c476a03f742c215f9689f283
Jadenmaxi15 <68132867+Jadenmaxi15@users.noreply.github.com>
2023-08-07 01:33:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scrolling for navigation links
     const navLinks = document.querySelectorAll("nav ul li a");
     for (const link of navLinks) {
         link.addEventListener("click", smoothScroll);
     }
-    
-    // Trigger fade-in animation for project items
+
     const projects = document.querySelectorAll(".project");
     for (const project of projects) {
         project.classList.add("fade-in");
     }
 
-    // Activate fade-in animation on scroll
     const fadeElems = document.querySelectorAll(".fade-in");
     const fadeOptions = {
         rootMargin: "0px 0px -50px 0px",
