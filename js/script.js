cab4534a0ad8de62a650ce4124505c95405ef762
Jadenmaxi15 <68132867+Jadenmaxi15@users.noreply.github.com>
2023-08-07 01:18:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scrolling for navigation links
     const navLinks = document.querySelectorAll("nav ul li a");
     for (const link of navLinks) {
         link.addEventListener("click", smoothScroll);
     }
+    
+    // Trigger fade-in animation for project items
+    const projects = document.querySelectorAll(".project");
+    for (const project of projects) {
+        project.classList.add("fade-in");
+    }
+
+    // Activate fade-in animation on scroll
+    const fadeElems = document.querySelectorAll(".fade-in");
+    const fadeOptions = {
+        rootMargin: "0px 0px -50px 0px",
+        threshold: 0
+    };
+
+    const fadeObserver = new IntersectionObserver(function (entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                fadeObserver.unobserve(entry.target);
+            }
+        });
+    }, fadeOptions);
+
+    fadeElems.forEach(elem => {
+        fadeObserver.observe(elem);
+    });
 });
 
 function smoothScroll(event) {
@@ -16,32 +43,3 @@ function smoothScroll(event) {
         });
     }
 }
-
-// Trigger fade-in animation for project items
-document.addEventListener("DOMContentLoaded", function () {
-    const projects = document.querySelectorAll(".project");
-    for (const project of projects) {
-        project.classList.add("fade-in");
-    }
-});
-
-// Activate fade-in animation on scroll
-const fadeElems = document.querySelectorAll(".fade-in");
-const fadeOptions = {
-    rootMargin: "0px 0px -50px 0px",
-    threshold: 0
-};
-
-const fadeObserver = new IntersectionObserver(function (entries) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("active");
-            fadeObserver.unobserve(entry.target);
-        }
-    });
-}, fadeOptions);
-
-fadeElems.forEach(elem => {
-    fadeObserver.observe(elem);
-});
-                  
